d6260defc39adc5e70251123191656e8800dc5ec
Shashank <s_shashank@outlook.com>
2018-09-08 17:13:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const firebase = require('firebase');
+    const config = {
+      apiKey: "AIzaSyBVpbW9t-YiI0nwGPw2ajPK5Ul3gfafQlo",
+      authDomain: "reactclientpanel-d4c01.firebaseapp.com",
+      databaseURL: "https://reactclientpanel-d4c01.firebaseio.com",
+      projectId: "reactclientpanel-d4c01",
+      storageBucket: "reactclientpanel-d4c01.appspot.com",
+      messagingSenderId: "228226322985"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App;
